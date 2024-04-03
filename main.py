b637e364cd09eee16e34ac9e13cfc7d79c73d4a6
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-03 17:06:09 -0300

    feat: removing the need to update in a separate file and implementing version.txt absence checker

--- main.py
+++ main.py
@@ -11,11 +11,51 @@ set_console_size()
 
 def check_for_updates():
     try:
-        exec(open("update.py").read())
-    except Exception as e:
-        print(f"\nErro ao verificar atualizações: \n{e}")
-        
-    input("\nPressione qualquer tecla para continuar...")
+        # URL do arquivo no GitHub que contém a versão mais recente do CLI
+        version_url = "https://raw.githubusercontent.com/zVitorSantos/Precificador/main/version.txt"
+
+        # Faz uma solicitação GET para obter a versão mais recente
+        response = requests.get(version_url)
+        latest_version = response.text.strip()
+
+        try:
+            # Tenta ler a versão atual do arquivo version.txt local
+            with open("version.txt", "r") as file:
+                current_version = file.read().strip()
+        except FileNotFoundError:
+            # Se o arquivo version.txt não for encontrado, define a versão atual como 0
+            current_version = "0"
+
+        # URL do arquivo .exe no GitHub que você deseja baixar se houver uma nova versão
+        file_url = f"https://github.com/zVitorSantos/Precificador/releases/download/v{latest_version}/main.exe"
+
+        # Compara a versão mais recente com a versão atual
+        if latest_version > current_version:
+            print("Uma nova versão está disponível. Atualizando...")
+
+            # Faz uma solicitação GET para baixar o novo arquivo .exe
+            response = requests.get(file_url)
+
+            # Substitui o antigo arquivo .exe pelo novo
+            with open("main_new.exe", "wb") as file:
+                file.write(response.content)
+
+            # Substitui o antigo arquivo .exe pelo novo
+            os.rename("main_new.exe", "main.exe")
+
+            # Atualiza a current_version no script
+            current_version = latest_version
+
+            # Atualiza a current_version no arquivo version.txt
+            with open("version.txt", "w") as file:
+                file.write(current_version)
+
+            print("Atualização concluída.")
+        else:
+            print("Versão mais recente.")
+            
+    except requests.exceptions.RequestException as e:
+        print(f"Erro ao verificar atualizações: {e}")
 
 check_for_updates()
 
