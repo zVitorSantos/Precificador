641216675f4c41dbef3b2ac2f01765f4bf4e3fc6
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-04 13:08:38 -0300

    feat: build script created

---
+++ build.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import subprocess
+
+# Cria os executáveis
+subprocess.run(["py", "-m", "PyInstaller", "--onefile", "--icon=main.ico", "--specpath", "./spec", "--distpath", "./dist/main", "--workpath", "./build/main", "main.py"])
+subprocess.run(["py", "-m", "PyInstaller", "--onefile", "--icon=update.ico", "--specpath", "./spec", "--distpath", "./dist/update", "--workpath", "./build/update", "update.py"])
+
+# Cria o diretório Precificador se ele não existir
+os.makedirs("Precificador", exist_ok=True)
+
+# Copia os executáveis e o version.txt para o diretório Precificador
+shutil.copy("./dist/main/main.exe", "./Precificador")
+shutil.copy("./dist/update/update.exe", "./Precificador")
+shutil.copy("./version.txt", "./Precificador")
+
+# Lê a versão do arquivo version.txt
+with open("version.txt", "r") as file:
+    version = file.read().strip()
+
+# Cria um arquivo zip do diretório Precificador com a versão no nome
+shutil.make_archive(f"Precificador-v{version}", 'zip', "./Precificador")
+
+# Exclui o diretório Precificador
+shutil.rmtree("Precificador")
--- readme.md
+++ readme.md
@@ -1,4 +1,1 @@
-
-Comando para fazer o executável
-"py -m PyInstaller --onefile --specpath ./spec --distpath ./dist/main --workpath ./build/main main.py"
-"py -m PyInstaller --onefile --specpath ./spec --distpath ./dist/update --workpath ./build/update update.py"
+Para rodar somente iniciar o create.py
