a32f00d6a48b9b90962daf059301bd0719408bf6
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-04 13:40:51 -0300

    feat: build updated to optmize paths

--- build.py
+++ build.py
@@ -6,20 +6,20 @@ import subprocess
 subprocess.run(["py", "-m", "PyInstaller", "--onefile", "--icon=main.ico", "--specpath", "./spec", "--distpath", "./dist/main", "--workpath", "./build/main", "main.py"])
 subprocess.run(["py", "-m", "PyInstaller", "--onefile", "--icon=update.ico", "--specpath", "./spec", "--distpath", "./dist/update", "--workpath", "./build/update", "update.py"])
 
-# Cria o diretório Precificador se ele não existir
-os.makedirs("Precificador", exist_ok=True)
-
-# Copia os executáveis e o version.txt para o diretório Precificador
-shutil.copy("./dist/main/main.exe", "./Precificador")
-shutil.copy("./dist/update/update.exe", "./Precificador")
-shutil.copy("./version.txt", "./Precificador")
-
 # Lê a versão do arquivo version.txt
 with open("version.txt", "r") as file:
     version = file.read().strip()
 
-# Cria um arquivo zip do diretório Precificador com a versão no nome
-shutil.make_archive(f"Precificador-v{version}/Precificador", 'zip', ".")
+# Cria o diretório Precificador v{version} dentro de build se ele não existir
+os.makedirs(f"./build/Precificador v{version}", exist_ok=True)
 
-# Exclui o diretório Precificador
-shutil.rmtree("Precificador")
+# Copia os executáveis e o version.txt para o diretório Precificador v{version}
+shutil.copy("./dist/main/main.exe", f"./build/Precificador v{version}")
+shutil.copy("./dist/update/update.exe", f"./build/Precificador v{version}")
+shutil.copy("./version.txt", f"./build/Precificador v{version}")
+
+# Cria um arquivo zip do diretório Precificador v{version} com o mesmo nome
+shutil.make_archive(f"./dist/Precificador v{version}", 'zip', "./build", f"Precificador v{version}")
+
+# Exclui o diretório Precificador v{version}
+shutil.rmtree(f"./build/Precificador v{version}")
