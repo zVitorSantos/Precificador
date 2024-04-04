08f443abff445cbfe87d3c449ae7092b354a8925
zVitorSantos <vitorsantos4736@gmail.com>
2024-04-04 14:20:15 -0300

    fix: guess what? yes, build+update fix

--- build.py
+++ build.py
@@ -9,17 +9,24 @@ subprocess.run(["py", "-m", "PyInstaller", "--onefile", "--icon=update.ico", "--
 # Lê a versão do arquivo version.txt
 with open("version.txt", "r") as file:
     version = file.read().strip()
+    
+# Caminho para o arquivo zip
+zip_file_path = f"./dist/Precificador-v{version}.zip"
+
+# Verifica se o arquivo zip já existe e, em caso afirmativo, exclui-o
+if os.path.exists(zip_file_path):
+    os.remove(zip_file_path)
 
 # Cria o diretório Precificador v{version} dentro de dist se ele não existir
 os.makedirs(f"./dist/Precificador-v{version}", exist_ok=True)
 
 # Copia os executáveis e o version.txt para o diretório Precificador v{version}
-shutil.copy("./dist/main.exe", f"./dist/Precificador v{version}")
-shutil.copy("./dist/update.exe", f"./dist/Precificador v{version}")
-shutil.copy("./version.txt", f"./dist/Precificador v{version}")
+shutil.copy("./dist/main.exe", f"./dist/Precificador-v{version}")
+shutil.copy("./dist/update.exe", f"./dist/Precificador-v{version}")
+shutil.copy("./version.txt", f"./dist/Precificador-v{version}")
 
 # Cria um arquivo zip do diretório Precificador v{version} com o mesmo nome
-shutil.make_archive(f"./dist/Precificador v{version}", 'zip', "./dist", f"Precificador v{version}")
+shutil.make_archive(f"./dist/Precificador-v{version}", 'zip', "./dist", f"Precificador-v{version}")
 
 # Exclui o diretório Precificador v{version}
 shutil.rmtree(f"./dist/Precificador-v{version}")
